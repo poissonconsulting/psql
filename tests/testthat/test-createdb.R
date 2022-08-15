2775add8324fce30c563d0450b7e44664556dddd
Ayla Pearson <ayla@poissonconsulting.ca>
2022-08-15 16:30:21 -0700

    adding create database tests

--- tests/testthat/test-createdb.R
+++ tests/testthat/test-createdb.R
@@ -1,3 +1,14 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# run DROP DATABASE dbname;
+
+test_that("creates new database", {
+  skip_on_ci()
+  expect_true(psql_createdb("newdb"))
+})
+
+test_that("errors when no dbname passed", {
+  skip_on_ci()
+  expect_error(
+    psql_createdb(),
+    regexp = 'argument "dbname" is missing, with no default'
+  )
 })
