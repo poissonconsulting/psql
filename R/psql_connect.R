b35554126dfa8738979047fe1fb6ae09f2ac5c93
Ayla Pearson <ayla@poissonconsulting.ca>
2022-08-15 14:38:02 -0700

    updating connect docs

--- R/psql_connect.R
+++ R/psql_connect.R
@@ -1,30 +1,39 @@
 #' Connect to PostgreSQL Database
 #'
-#' Connect to the default local database or a specific database by supplying
-#' the file path for a config.yml file.
+#' Connect to a database, either supply connection details though a config file
+#'  or connect to local default database.
 #'
 #' @param config_path A string of a file path to the yaml configuration file.
 #' The default value grabs the file path from the psql.config_path option.
 #' @param value A string of the name of value. The default value grabs the value
 #' from the psql.value option.
 #'
-#' @return Returns a postgres connection
-#' @details The yml file will use contain connection details for: host, port,
+#' @return Returns a database connection
+#' @details The yml file can contain connection details for: host, port,
 #'  dbname, user and password. The host, database name, user name and password
 #'  should be passed as string. The port is passed as an integer.
 #'  The function uses `config::get()` to read the config file, check there for
 #'  more information on reading in the config data.
 #'
-#'  If no config details are passed through it will connect to your local
-#'  cluster.
+#'  If no config details are passed it will connect to your local cluster.
+#'
+#'  Set the values for `psql.config_path` and `psql.value` for the function
+#'   to grab config details.
+#'   The values can be set `options()`.
 #'
 #' @examples
 #' \dontrun{
-#' psql_connect()
+#' conn <- psql_connect()
+#' DBI::dbDisconnect(conn)
+#'
+#' psql_connect("config.yml")
+#' DBI::dbDisconnect(conn)
+#'
 #' psql_connect(config_path = "config.yml", value = "database")
+#' DBI::dbDisconnect(conn)
 #' }
 #'
-psql_connect <- function(config_path = getOption("psql.config", NULL),
+psql_connect <- function(config_path = getOption("psql.config_path", NULL),
                          value = getOption("psql.value", NULL)) {
   chk::chk_null_or(config_path, vld = chk::vld_string)
   chk::chk_null_or(value, vld = chk::vld_string)
