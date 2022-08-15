49b09206a2e25b6602c700d5af8f65754687bccd
Ayla Pearson <ayla@poissonconsulting.ca>
2022-08-15 14:03:01 -0700

    writing tests for connect function

--- tests/testthat/test-psql_connect.R
+++ tests/testthat/test-psql_connect.R
@@ -10,5 +10,25 @@ test_that("working local connection when nothing supplied", {
   expect_s4_class(conn, "PqConnection")
 })
 
-# could create blank .yml file withr and ensure it works
-#
+test_that("working local connection of a different db from config", {
+  path <- system.file("testhelpers/config.yml", package = "psql")
+  conn <- psql_connect(config_filepath = path)
+  on.exit(DBI::dbDisconnect(conn))
+  expect_s4_class(conn, "PqConnection")
+})
+
+test_that("working local connection of a different db from config other value", {
+  path <- system.file("testhelpers/config-value.yml", package = "psql")
+  conn <- psql_connect(config_filepath = path, value = "database")
+  on.exit(DBI::dbDisconnect(conn))
+  expect_s4_class(conn, "PqConnection")
+})
+
+### HACK - delete when confidence or something
+test_that("working hosted to test db", {
+  skip_on_ci()
+  path <- system.file("testhelpers/config-hosted.yml", package = "psql")
+  conn <- psql_connect(config_filepath = path)
+  on.exit(DBI::dbDisconnect(conn))
+  expect_s4_class(conn, "PqConnection")
+})
