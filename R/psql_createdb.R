141fbb9b770a3fcb787c0e03863361b0f348f173
Ayla Pearson <ayla@poissonconsulting.ca>
2022-08-15 15:43:04 -0700

    creating createdb function

--- R/psql_createdb.R
+++ R/psql_createdb.R
@@ -8,8 +8,15 @@
 #' @export
 #'
 #' @examples
-psql_createdb <- function(dbname, config, value) {
-  cmd <- paste("createdb", dbname)
-  system(cmd)
+psql_createdb <- function(dbname,
+                          config_path = getOption("psql.config_path", NULL),
+                          config_value = getOption("psql.value", NULL)) {
+
+  conn <- psql_connect(config_path, config_value)
+  on.exit(DBI::dbDisconnect(conn))
+
+  cmd <- paste0("CREATE DATABASE ", dbname, ";")
+  DBI::dbSendQuery(conn, cmd)
+
   TRUE
 }
