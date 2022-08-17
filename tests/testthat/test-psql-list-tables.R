f5ea7fca42cb2a2108a177ab58711da645fc645b
Ayla Pearson <ayla@poissonconsulting.ca>
2022-08-16 19:53:09 -0700

    adding test for listing tables

--- tests/testthat/test-psql-list-tables.R
+++ tests/testthat/test-psql-list-tables.R
@@ -1,9 +1,38 @@
-test_that("list tables", {
+test_that("list local tables", {
+  skip_on_ci()
+  # set up test
   config_path <- system.file("testhelpers/config.yml", package = "psql")
-
-  psql_list_tables(
-    "input",
+  psql_execute_db("CREATE SCHEMA boat_count", config_path = config_path)
+  psql_execute_db(
+    "CREATE TABLE boat_count.input (
+     x INTEGER NOT NULL,
+     y INTEGER)",
+    config_path = config_path
+  )
+  psql_execute_db(
+    "CREATE TABLE boat_count.counts (
+     x INTEGER NOT NULL,
+     y INTEGER)",
+    config_path = config_path
+  )
+  # execute tests
+  output <- psql_list_tables(
     "boat_count",
+    config_path = config_path
+  )
+
+  expect_equal(output, c("input", "counts"))
+  # clean up
+  psql_execute_db(
+    "DROP TABLE boat_count.input",
+    config_path = config_path
+  )
+  psql_execute_db(
+    "DROP TABLE boat_count.counts",
+    config_path = config_path
+  )
+  psql_execute_db(
+    "DROP SCHEMA boat_count",
     config_path = config_path
   )
 })
