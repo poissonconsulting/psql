30230eaecef15df7fa9d56d81bef02b56415e5e5
Ayla Pearson <ayla@poissonconsulting.ca>
2022-08-17 13:55:38 -0700

    docs

--- R/params.R
+++ R/params.R
@@ -9,7 +9,6 @@
 #' The default value grabs the file path from the psql.config_path option.
 #' @param config_value A string of the name of value. The default value grabs the value
 #' from the psql.value option.
-#' @param tbl_name A string indicating the name of the table. Default is to use
-#'  the name of the `tbl` argument.
+#' @param tbl_name A string indicating the name of the table.
 #' @param schema A string of the schema name. Default value is `"public"`.
 NULL
